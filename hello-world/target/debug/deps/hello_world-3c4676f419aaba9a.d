0e8621723f207391ce50c4c3f47c1f6f0bc45607
koukyo1994 <arabiannight1994@yahoo.co.jp>
2019-07-24 00:08:58 +0900

    deleted o files

--- hello-world/target/debug/deps/hello_world-3c4676f419aaba9a.d
+++
@@ -1,5 +0,0 @@
-/Users/hidehisa/Hobby/rust-sample/hello-world/target/debug/deps/hello_world-3c4676f419aaba9a: src/main.rs
-
-/Users/hidehisa/Hobby/rust-sample/hello-world/target/debug/deps/hello_world-3c4676f419aaba9a.d: src/main.rs
-
-src/main.rs:
--- hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-/Users/hidehisa/Hobby/rust-sample/hello-world/target/debug/hello-world.dSYM: /Users/hidehisa/Hobby/rust-sample/hello-world/src/main.rs
